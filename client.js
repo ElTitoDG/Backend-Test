9d2ec9f43fda77eead1de44c83579cc378fb50f9
Julian <julianhinojosagil@gmail.com>
2021-09-07 16:20:34 +0200

    Basic Chat V3

--- client.js
+++ client.js
@@ -1,20 +1,66 @@
 const { Socket } = require("net");
 const readline = require("readline");
 
-const chat = readline.createInterface({
+const input = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const socket = new Socket();
+const END = "END";
 
-socket.connect({ host: "localhost", port: 3000 });
-socket.setEncoding("utf-8");
+const error = (message) => {
+  console.error(message);
+  process.exit(1);
+};
 
-chat.on("line", (line) => {
-  socket.write(line);
-});
+const connect = (host, port) => {
+  console.log(`Connecting to ${host}:${port}`);
 
-socket.on("data", (data) => {
-  console.log(data);
-});
+  const socket = new Socket();
+  socket.connect({ host, port });
+  socket.setEncoding("utf-8");
+
+  socket.on("connect", () => {
+    console.log("Connected");
+
+    input.question("Choose your usename: ", (username) => {
+      socket.write(username);
+      console.log(`Type any message to send it, type ${END} to finish chat`);
+    });
+
+    input.on("line", (message) => {
+      socket.write(message);
+      if (message === END) {
+        socket.end();
+        console.log("Disconnected");
+      }
+    });
+
+    socket.on("data", (data) => {
+      console.log(data);
+    });
+  });
+
+  socket.on("error", (err) => error(err.message));
+  socket.on("close", () => process.exit(0));
+};
+
+const main = () => {
+  if (process.argv.length !== 4) {
+    error(`Usage: node ${__filename} host port`);
+  }
+
+  let [, , host, port] = process.argv;
+  if (isNaN(port)) {
+    error(`Invalid port: ${port}`);
+  }
+  port = Number(port);
+
+  connect(host, port);
+};
+
+if (module === require.main) {
+  main();
+}
+
+
